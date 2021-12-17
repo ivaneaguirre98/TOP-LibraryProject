bc7528004110cf6642ea2732e01571ab2bc028b7
Ivan <aguirreivan96@gmail.com>
2021-12-16 22:19:53 -0600

    Added button and connected it to a addBook function in JS

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let myLibrary = [];
 
+let addButt = document.getElementById("addBookButton");
+addButt.addEventListener("click", addBook);
+
 function Book(author, title, pageNum, isRead){
     this.author = author,
     this.title = title,
@@ -13,6 +16,10 @@ let harryPotter = new Book("Smith John", "Harry Potter", 100, true);
 
 function addToArray(bookObj){
     myLibrary.push(bookObj);
+}
+
+function addBook(){
+    console.log("Button Pressed")
 }
 
 addToArray(theHobbit);
